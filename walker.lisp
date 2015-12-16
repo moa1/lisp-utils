72a143a4e99de3143316bf52467d5606039dfa69
toni <toni@pc1400>
2015-12-16 04:20:47 +0100

    add #'PARSE-DECLARATION-IN-BODY which parses possible declarations in a body and returns the rest of the body and the parsed declarations.
    add slot FREEP to class SYM that indicates whether a variable is free or bound.
    rename slot TYPE in class SYM to DECLSPECS. Its new meaning is the list of DECLSPECs that apply to the symbol.
    rename class FORM-WITH-BODY to BODY-FORM to be consistent with naming of the other classes.
    add a new class BINDINGS that encapsulates a binding in LET, LET*. rename slot DEFINITION of classes LET-FORM and LET*-FORM to BINDINGS.
    add a slot DECLSPECS to classes LET-FORM, LET*-FORM.
    change #'PARSE to create BINDINGs instead of a list of SYMs and add parsing declarations in forms LET and LET*.

--- walker.lisp
+++ walker.lisp
@@ -35,6 +35,11 @@
 ;; labels     progv                                  
 ;; let        quote
 
+;; CLHS 3.8 Symbol DECLARE
+;; dynamic-extent  ignore     optimize  
+;; ftype           inline     special   
+;; ignorable       notinline  type      
+
 (ql:quickload :alexandria)
 
 (defpackage :walker
@@ -53,15 +58,22 @@
   ;; types ;i.e. class- or structure-names
   )
 
-(defclass sym ()
-  ((name :initarg :name :accessor sym-name) ;NIL if not known
-   (definition :initarg :definition :accessor sym-definition) ;NIL if not known
-   (type :initarg :type :accessor sym-type) ;the declared type, NIL if not known
-   ))
+;;;; NAMESPACES
 
+(defclass sym ()
+  ((name :initarg :name :accessor sym-name :type symbol
+	 :documentation "NIL if not known")
+   (freep :initarg :freep :accessor sym-freep :type boolean
+	  :documentation "T if it is a free variable/function, or NIL if bound. Note that this is specific to a namespace.")
+   (definition :initarg :definition :accessor sym-definition ;;TODO uncomment when llist is implemented :typep (or binding llist)
+	       :documentation "the parsed object (of type (OR BINDING LLIST)) it is defined in, NIL if not known")
+   (declspecs :initarg :declspecs :accessor sym-declspecs :type list
+	      :documentation "a list of DECLSPECs that apply to this symbol")
+   )
+  (:documentation "A symbol referring to a variable or function.
+Note that symbols are always parsed in a lexical manner, regardless of whether the actual variable this symbol is referring to may be a lexical or special variable. For example, the symbols *A* in the form (LET ((*A* 1)) *A*) may refer to a lexical or special variable."))
 (defclass var (sym)
   ())
-
 (defclass fun (sym)
   ())
 
@@ -103,59 +115,165 @@ Example: (augment-namespace 'var-a (make-instance 'var :name 'var-a) (make-names
 		(let ((str (make-array '(0) :element-type 'base-char :fill-pointer 0 :adjustable t)))
 		  (with-output-to-string (output str)
 		    (print-unreadable-object (sym output :type t :identity t)
-		      (format output "NAME:~A DEFINITION:~A TYPE:~A" (sym-name sym) (sym-definition sym) (sym-type sym))))
+		      (format output "NAME:~A FREEP:~A DEFINITION:~A DECLSPECS:~A" (sym-name sym) (sym-freep sym) (sym-definition sym) (sym-declspecs sym))))
 		  str)))
 	 (format stream "~A: ~A~%" (car cell) (format-sym (cdr cell))))))
+
+;;;; DECLARATIONS
+
+(defclass declspec ()
+  ((parent :initarg :parent :accessor declspec-parent)))
+(defclass declspec-type (declspec)
+  ((type :initarg :type :accessor declspec-type)
+   (vars :initarg :vars :accessor declspec-vars :type list)))
+
+(defun parse-declaration-in-body (body variables functions parent &key customparsep-function customparse-function)
+  "Parses declarations in the beginning of BODY.
+Returns two values: the rest of the BODY that does not start with a DECLARE-expression, and a list of DECLSPEC-objects.
+Side-effects: Adds references of the created DECLSPEC-objects to the DECLSPEC-slots of VARIABLES and FUNCTIONS."
+  (declare (optimize (debug 3)))
+  (assert (listp body) () "Malformed BODY:~%~A" body)
+  (labels ((lexboundp (symbol)
+	     (namespace-boundp symbol variables))
+	   (lexfboundp (symbol)
+	     (namespace-boundp symbol functions))
+	   (varlookup/create (symbol)
+	     (if (lexboundp symbol)
+		 (namespace-lookup symbol variables)
+		 (make-instance 'var :name symbol :freep t :declspecs nil))) ;do not bind :DEFINITION
+	   (funlookup/create (symbol)
+	     (if (lexfboundp symbol)
+		 (namespace-lookup symbol functions)
+		 (make-instance 'fun :name symbol :freep t :declspecs nil))) ;do not bind :DEFINITION
+	   (parse-declspec-type (expr)
+	     "parse a single declaration-specifier.
+Example: (parse-declare '(type fixnum a b c) nil nil nil)"
+	     (assert (and (listp expr) (symbolp (car expr)) (listp (cdr expr))) () "Cannot parse declaration-specifier: ~A" expr)
+	     (let ((head (car expr))
+		   (rest (cdr expr)))
+	       (cond
+		 ((eq head 'type)
+		  (assert (and (listp rest)))
+		  ;;(let ((typespec 
+		  ))))
+	   (parse-declspecs (declspecs collected-declspecs)
+	     "Example: (parse-declspecs '(type fixnum a b c) nil)"
+	     (cond
+	       ((null declspecs)
+		(nreverse collected-declspecs))
+	       (t
+		(let* ((expr (car declspecs))
+		       (identifier (car expr))
+		       (body (cdr expr)))
+		  (assert (symbolp identifier) () "Malformed declaration identifier ~A" identifier)
+		  (let ((parsed-declspec
+			 (cond
+			   ((and (not (null customparsep-function)) (funcall customparsep-function identifier body variables functions parent))
+			    (funcall customparse-function identifier body variables functions parent))
+			   ((eq identifier 'type)
+			    (assert (and (listp body) (listp (cdr body))) () "Malformed TYPE declaration: ~A" expr)
+			    (let* ((typespec (car body))
+				   (vars (cdr body)))
+			      ;; will not check TYPESPEC, has to be done in user code.
+			      (let* ((parsed-vars (loop for var in vars collect (varlookup/create var)))
+				     (parsed-declspec (make-instance 'declspec-type :parent parent :type typespec :vars parsed-vars)))
+				(loop for var in parsed-vars do
+				     (push parsed-declspec (sym-declspecs var)))
+				parsed-declspec)))
+			   (t (error "Unknown declaration specifier ~A" expr)))))
+		    (parse-declspecs (cdr declspecs) (cons parsed-declspec collected-declspecs)))))))
+	   (parse-declare (body collected-declspecs)
+	     (let ((head (car body))
+		   (rest (cdr body)))
+	       (cond
+		 ((and (listp head) (eq (car head) 'declare))
+		  (let* ((declspecs (cdr head)))
+		    (parse-declare rest (parse-declspecs declspecs collected-declspecs))))
+		 (t (values body (nreverse collected-declspecs)))))))
+    (parse-declare body nil)))
+
+(multiple-value-bind (body declspecs) (parse-declaration-in-body '(declare (type fixnum)) nil nil nil)
+  (assert (and (equal body '(declare (type fixnum))) (null declspecs))))
+;;TODO: test that (parse-declaration-in-body '((declare ()) 5) nil nil nil) throws an error.
+(multiple-value-bind (body declspecs) (parse-declaration-in-body '((declare (type fixnum a)) 5) nil nil nil)
+  (assert (and (equal body '(5)) (typep (car declspecs) 'declspec-type))))
+(assert (sym-freep (car (declspec-vars (car (nth-value 1 (parse-declaration-in-body '((declare (type fixnum a)) 5) nil nil nil)))))))
+
+;;;; FORMS
 
 (defclass form ()
   ((parent :initarg :parent :accessor form-parent)))
 (defclass constant-form (form)
   ((value :initarg :value :accessor constant-value)))
-(defclass form-with-body ()
+(defclass body-form ()
   ((body :initarg :body :accessor form-body)))
 (defclass special-form (form)
   ())
-(defclass progn-form (special-form form-with-body)
+(defclass progn-form (special-form body-form)
   ())
-(defclass let-form (special-form form-with-body)
-  ((definitions :initarg :definitions :accessor form-definitions)))
-(defclass let*-form (special-form form-with-body)
-  ((definitions :initarg :definitions :accessor form-definitions)))
+(defclass binding ()
+  ((parent :initarg :parent :accessor binding-parent)
+   (sym :initarg :sym :accessor binding-sym :type sym)
+   (definition :initarg :definition :accessor binding-definition)))
+(defclass var-binding (binding)
+  ())
+(defclass let-form (special-form body-form)
+  ((bindings :initarg :bindings :accessor form-bindings :type list) ;list of VAR-BINDINGs
+   (declspecs :initarg :declspecs :accessor form-declspecs :type list))) ;list of DECLSPECs
+(defclass let*-form (special-form body-form)
+  ((bindings :initarg :bindings :accessor form-bindings :type list) ;list of VAR-BINDINGs
+   (declspecs :initarg :declspecs :accessor form-declspecs :type list))) ;list of DECLSPECs
+;; (defclass fun-binding (binding)
+;;   ())
+;; (defclass function-form (body-form declare-form)
+;;   ((llist :initarg :llist :accessor form-llist :type llist))) 
+;; (defclass flet-form (special-form body-form declare-form)
+;;   ((bindings :initarg :bindings :accessor form-bindings :type list) ;list of FUN-BINDINGs, with (type-of (binding-definition (car LIST-of-FUN-BINDINGs)))==FUNCTION-FORM
+;;    (declspecs :initarg :declspecs :accessor form-declspecs :type list))) ;list of DECLSPECs
+;; (defclass labels-form (special-form body-form declare-form)
+;;   ((bindings :initarg :bindings :accessor form-bindings :type list) ;list of FUN-BINDINGs, with (type-of (binding-definition (car LIST-of-FUN-BINDINGs)))==FUNCTION-FORM
+;;    (declspecs :initarg :declspecs :accessor form-declspecs :type list))) ;list of DECLSPECs
 
 (defmethod print-object ((object constant-form) stream)
   (print-unreadable-object (object stream :type t)
     (format stream "~A" (constant-value object))))
-(defmethod print-object ((object form-with-body) stream)
+(defmethod print-object ((object body-form) stream)
   (print-unreadable-object (object stream :type t :identity t)
     (format stream "~A" (form-body object))))
+(defmethod print-object ((object declspec-type) stream)
+  (print-unreadable-object (object stream :type t :identity nil)
+    (format stream "TYPE:~A VARS:~A" (declspec-type object) (declspec-vars object))))
 
-(defun parse (form variables functions parent &key interruptp-function interrupt-function)
+;; TODO: maybe rename to PARSE-FORM.
+(defun parse (form variables functions parent &key customparsep-function customparse-function customparsedeclspecp-function customparsedeclspec-function)
   (declare (optimize (debug 3)))
   (labels ((lexboundp (symbol)
 	     (namespace-boundp symbol variables))
 	   (lexfboundp (symbol)
 	     (namespace-boundp symbol functions))
-	   (varlookup (symbol)
+	   (varlookup/create (symbol)
 	     (if (lexboundp symbol)
 		 (namespace-lookup symbol variables)
-		 (make-instance 'var :name symbol :definition nil)))
-	   (funlookup (symbol)
-	     (if (lexboundp symbol)
-		 (namespace-lookup symbol variables)
-		 (make-instance 'fun :name symbol :definition nil)))
+		 (make-instance 'var :name symbol :freep t :declspecs nil))) ;do not bind :DEFINITION
+	   (funlookup/create (symbol)
+	     (if (lexfboundp symbol)
+		 (namespace-lookup symbol functions)
+		 (make-instance 'fun :name symbol :freep t :declspecs nil))) ;do not bind :DEFINITION
 	   (reparse (form parent &key (variables variables) (functions functions))
 	     (parse form variables functions parent
-		    :interruptp-function interruptp-function
-		    :interrupt-function interrupt-function)))
+		    :customparsep-function customparsep-function
+		    :customparse-function customparse-function
+		    :customparsedeclspecp-function customparsedeclspecp-function
+		    :customparsedeclspec-function customparsedeclspec-function)))
     (cond
-      ((and (not (null interruptp-function)) (funcall interruptp-function form variables functions parent))
-       (funcall interrupt-function form variables functions parent))
+      ((and (not (null customparsep-function)) (funcall customparsep-function form variables functions parent))
+       (funcall customparse-function form variables functions parent))
       ((or (eq form nil) (eq form t))
        (make-instance 'constant-form :value form :parent parent))
       ((symbolp form)
-       (varlookup form))
+       (varlookup/create form))
       ((and (consp form) (eq (car form) 'function) (consp (cdr form)) (symbolp (cadr form)) (null (cddr form)))
-       (funlookup form))
+       (funlookup/create form))
       ((atom form)
        (make-instance 'constant-form :value form :parent parent))
       (t
@@ -170,61 +288,84 @@ Example: (augment-namespace 'var-a (make-instance 'var :name 'var-a) (make-names
 	      (setf (form-body current) parsed-body)
 	      current))
 	   ((or (eq head 'let) (eq head 'let*)) ;TODO: extend this case to LABELS and FLET.
-	    (assert (and (consp rest) (listp (car rest))) () "cannot parse LET-form:~%~A" form)
+	    (assert (and (consp rest) (listp (car rest))) () "cannot parse ~A-form:~%~A" head form)
 	    (let* ((definitions (car rest))
 		   (body (cdr rest))
-		   (current (make-instance 'let-form :parent parent :definitions nil :body nil)))
-	      (flet ((parse-definition (def variables)
+		   (form-type (ecase head ((let) 'let-form) ((let*) 'let*-form)))
+		   (current (make-instance form-type :parent parent :body nil))) ;(:BINDINGS :DECLSPECS) are defined below
+	      (flet ((parse-var-binding (def variables)
 		       (assert (and (consp def) (symbolp (car def)) (not (null (car def))) (not (null (cdr def))) (null (cddr def))) () "cannot parse definition in ~A-form:~%~A" head def)
 		       (let* ((name (first def))
 			      (value-form (second def))
-			      (parsed-value (reparse value-form current :variables variables)))
-			 (make-instance 'var :name name :definition parsed-value :type nil))))
-		(multiple-value-bind (parsed-variables new-variables)
-		    (if (eq head 'let)
-			(let ((parsed-variables (loop for def in definitions collect (parse-definition def variables))))
-			  (values parsed-variables (augment-namespace-with-syms parsed-variables variables)))
-			(let* ((new-variables variables)
-			       (parsed-variables (loop for def in definitions collect
-						      (let ((sym (parse-definition def new-variables)))
-							(setf new-variables (augment-namespace-with-sym sym new-variables))
-							sym))))
-			  (values parsed-variables new-variables)))
-		  (let ((parsed-body (loop for form in body collect
-				     (reparse form current :variables new-variables))))
-		    (setf (form-definitions current) parsed-variables)
-		    (setf (form-body current) parsed-body)
-		    current)))))
+			      (parsed-value (reparse value-form current :variables variables))
+			      (sym (make-instance 'var :name name :freep nil :definition :undef :declspecs nil))
+			      (binding (make-instance 'var-binding :parent parent :sym sym :definition parsed-value)))
+			 (setf (sym-definition sym) binding)
+			 binding)))
+		(multiple-value-bind (parsed-bindings new-variables)
+		    (cond
+		      ((eq head 'let)
+		       (let* ((parsed-bindings (loop for def in definitions collect (parse-var-binding def variables)))
+			      (parsed-syms (loop for binding in parsed-bindings collect (binding-sym binding))))
+			 (values parsed-bindings (augment-namespace-with-syms parsed-syms variables))))
+		      ((eq head 'let*)
+		       (let* ((new-variables variables)
+			      (parsed-bindings (loop for def in definitions collect
+						    (let* ((parsed-binding (parse-var-binding def new-variables))
+							   (parsed-sym (binding-sym parsed-binding)))
+						      (setf new-variables (augment-namespace-with-sym parsed-sym new-variables))
+						      parsed-binding))))
+			 (values parsed-bindings new-variables)))
+		      (t (error "unknown HEAD")))
+		  (multiple-value-bind (body parsed-declspecs)
+		      (parse-declaration-in-body body new-variables functions parent :customparsep-function customparsedeclspecp-function :customparse-function customparsedeclspec-function)
+		    (let ((parsed-body (loop for form in body collect
+					    (reparse form current :variables new-variables))))
+		      (setf (form-bindings current) parsed-bindings)
+		      (setf (form-declspecs current) parsed-declspecs)
+		      (setf (form-body current) parsed-body)
+		      current))))))
 	   )))
       )))
 
 ;; tests for PARSE
-(flet ((lexical-namespaces-at-here (form)
-	 "Parse FORM and collect the lexical namespaces at the positions in FORM marked by the symbol -HERE-.
+(defun test-parse-symbol-reference ()
+  (declare (optimize (debug 3)))
+  (flet ((lexical-namespaces-at-here (form)
+	   "Parse FORM and collect the lexical namespaces at the positions in FORM marked by the symbol -HERE-.
 Returns two values: a list containing the lexical variable namespaces, and a list containing the lexical function namespaces."
-	 (let* ((variables-here-list nil)
-		(functions-here-list nil))
-	   (parse form nil nil nil
-		  :interruptp-function (lambda (form vars funs parent)
-					 (declare (ignore vars funs parent))
-					 (eq form '-here-))
-		  :interrupt-function (lambda (form variables functions parent)
-					(declare (ignorable form parent))
-					(push variables variables-here-list)
-					(push functions functions-here-list)))
-	   (values (nreverse variables-here-list) (nreverse functions-here-list)))))
-  (assert (let* ((variables (car (lexical-namespaces-at-here '(let ((b 5) (c b)) -here-))))
-		 (b-sym (namespace-lookup 'b variables))
-		 (c-sym (namespace-lookup 'c variables)))
-	    (not (eq b-sym (sym-definition c-sym)))))
-  (assert (let* ((variables (car (lexical-namespaces-at-here '(let* ((b 5) (c b)) -here-))))
-		 (b-sym (namespace-lookup 'b variables))
-		 (c-sym (namespace-lookup 'c variables)))
-	    (eq b-sym (sym-definition c-sym))))
-  (let* ((variables (lexical-namespaces-at-here '(let ((b 5)) -here- (let ((b b)) -here-))))
-	 (variables-1 (car variables))
-	 (variables-2 (cadr variables))
-	 (b-1 (namespace-lookup 'b variables-1))
-	 (b-2 (namespace-lookup 'b variables-2)))
-    (assert (eq (constant-value (sym-definition b-1)) 5))
-    (assert (eq b-1 (sym-definition b-2)))))
+	   (let* ((variables-here-list nil)
+		  (functions-here-list nil))
+	     (parse form nil nil nil
+		    :customparsep-function (lambda (form vars funs parent)
+					     (declare (ignore vars funs parent))
+					     (eq form '-here-))
+		    :customparse-function (lambda (form variables functions parent)
+					    (declare (ignorable form parent))
+					    (push variables variables-here-list)
+					    (push functions functions-here-list)))
+	     (values (nreverse variables-here-list) (nreverse functions-here-list)))))
+    (assert (sym-freep (parse 'a nil nil nil)))
+    (assert (sym-freep (parse '(function a) nil nil nil)))
+    (assert (not (sym-freep (car (form-body (parse '(let ((a 1)) a) nil nil nil))))))
+    ;;TODO uncomment when FLET is implemented: (assert (not (sym-freep (car (form-body (parse '(flet ((a 1)) #'a) nil nil nil))))))
+    (assert (let* ((variables (car (lexical-namespaces-at-here '(let ((b 5) (c b)) -here-))))
+		   (b-sym (namespace-lookup 'b variables))
+		   (c-sym (namespace-lookup 'c variables)))
+	      (not (eq b-sym (binding-definition (sym-definition c-sym))))))
+    (assert (let* ((variables (car (lexical-namespaces-at-here '(let* ((b 5) (c b)) -here-))))
+		   (b-sym (namespace-lookup 'b variables))
+		   (c-sym (namespace-lookup 'c variables)))
+	      (eq b-sym (binding-definition (sym-definition c-sym)))))
+    (let* ((variables (lexical-namespaces-at-here '(let ((b 5)) -here- (let ((b b)) -here-))))
+	   (variables-1 (car variables))
+	   (variables-2 (cadr variables))
+	   (b-1 (namespace-lookup 'b variables-1))
+	   (b-2 (namespace-lookup 'b variables-2)))
+      (assert (eq (constant-value (binding-definition (sym-definition b-1))) 5))
+      (assert (eq b-1 (binding-definition (sym-definition b-2))))))
+  (let* ((form '(let ((a 1)) (declare (type fixnum a)) a))
+	 (ast (parse form nil nil nil))
+	 (declspec-type (car (form-declspecs ast))))
+    (assert (equal (car (declspec-vars declspec-type)) (car (form-body ast))))))
+(test-parse-symbol-reference)
